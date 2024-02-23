cf84a80c1224a5579c87ecfde57610a5a406ea76
Navedita Katoch <katochna@sheridancollege.ca>
2024-02-23 16:37:06 -0500

    Wrote comments

---
+++ src/midtermreviewcodeforpartc/Navedita_Design.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermreviewcodeforpartc;
+
+/**
+ *
+ * @author user
+ */
+public class Navedita_Design {
+    // My favourite subject is Software Development.
+    // I am learning it in my semester 3.
+    // I also like Java and writing code.
+    // I am also learning about using GIT. 
+    // Python AI is also a interesting subject to learn.
+}
